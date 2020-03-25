6de61e4535642bcc7a43bc1aed177e83e94c79c5
Frankie Ian <frankie.ian@students.mq.edu.au>
2020-03-25 21:35:10 +1100

    Implemented parseLog function and new file extractLogs.

--- parse_logs.py
+++ parse_logs.py
@@ -2,11 +2,19 @@
 This python file will parse the log(s) of workloads and extract the data
 """
 
-# Function will parse a log
-def parseLog():
-    return 0
+# Function will parse logs
+# Returns log as a list
+# input files must be a list
+def parseLog(files):
+    logList = []
 
+    for file in files:
+        filepath = file
+        with open(filepath) as fp:
+            line = fp.readline()
+            while line and len(line) != 0:
+                if line[0] != ';':
+                    logList.append(line.split())
+                line = fp.readline()
 
-# Function will parse multiple log
-def parseLog():
-    return
+    return logList
