bc290ce9588b6b7a3bea00755513f48fb550b254
Frankie Ian <frankie.ian@students.mq.edu.au>
2020-03-25 17:44:30 +1100

    Add parse log file placeholder

---
+++ parse_logs.py
@@ -0,0 +1,12 @@
+"""
+This python file will parse the log(s) of workloads and extract the data
+"""
+
+# Function will parse a log
+def parseLog():
+    return 0
+
+
+# Function will parse multiple log
+def parseLog():
+    return
